a8dca614a427990d1926f4f84e5819b50d480f89
Ishani <ishaniamin@gmail.com>
2018-06-19 15:07:58 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -4,5 +4,17 @@ class River
   def initialize(name)
     @name = name
   end
-  
+
+  def length=(length)
+    @length = length
+  end
+
+  def countries=(countries)
+    @countries = countries
+  end
+
+  def discharge=(discharge)
+    @discharge = discharge
+  end
+
 end
